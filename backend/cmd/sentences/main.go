b068f79b1378a5c1eb57cfedba9f3df7cfea3831
Daniel Fryer <17fryerd@gmail.com>
2026-06-30 16:31:24 -0400

    update sentences script

--- backend/cmd/sentences/main.go
+++ backend/cmd/sentences/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"strconv"
 	"strings"
 
 	"github.com/pepe454/joman-dictionary/internal/csv"
@@ -20,6 +21,40 @@ const (
 	Sourashtra Language = "Sourashtra"
 )
 
+func identifyCorrectWord(ctx context.Context, q *repository.Queries, word string, language Language) (correctWord string, wordID int32) {
+	correctWord, wordID = "", -1
+	searchResults, _ := q.WordSearch(ctx, repository.WordSearchParams{Language: string(language), Similarity: word})
+	for i, result := range searchResults {
+		fmt.Printf("%d) Found similar word %s with ID %d\n", i+1, result.WordText, result.WordID)
+	}
+
+	// Let the user specify if they want to use a queried set of words or enter their own word.
+	var choiceStr string
+	fmt.Print("Enter the number of the correct word if listed above, -1 to enter correct word, or 0 to skip: ")
+	fmt.Scanln(&choiceStr)
+	choice, scanErr := strconv.Atoi(strings.TrimSpace(choiceStr))
+	if scanErr != nil {
+		fmt.Printf("Error reading input: %v\n", scanErr)
+	} else if choice == 0 {
+		fmt.Printf("Skipping word %s\n", word)
+	} else if choice > 0 {
+		if choice > len(searchResults)+1 {
+			fmt.Printf("Invalid choice %d. Needed to enter a number between 1 and %d.\n", choice, len(searchResults))
+		} else {
+			word := searchResults[choice-1]
+			correctWord, wordID = word.WordText, word.WordID
+		}
+	} else {
+		var correctWord string
+		fmt.Printf("Could not find similar word for %s. Please enter the correct matching word: ", word)
+		fmt.Scanln(&correctWord)
+		if len(correctWord) > 0 {
+			return identifyCorrectWord(ctx, q, correctWord, language)
+		}
+	}
+	return
+}
+
 func uploadSentence(
 	ctx context.Context, q *repository.Queries, wordMap map[string]int32,
 	sentence string, language Language) error {
@@ -29,6 +64,8 @@ func uploadSentence(
 	fmt.Print(header)
 
 	words := strings.SplitSeq(sentence, " ")
+	contextWordIDs := make([]int32, 0, 5)
+
 	for word := range words {
 		// strip the punctuation from the word
 		word = strings.Trim(word, ".,!?;:\"'()[]{}")
@@ -50,10 +87,18 @@ func uploadSentence(
 				break
 			}
 		}
+
+		// Try to get user input with a database search if we cannot identify the word.
 		if !ok {
-			fmt.Printf("Could not identify word %s\n", word)
+			fmt.Printf("Could not identify word: %s. Are any of the following correct?\n", word)
+			correctWord, wordID := identifyCorrectWord(ctx, q, word, language)
+			if wordID > 0 {
+				fmt.Printf("Found word %s with ID %d\n", correctWord, wordID)
+				contextWordIDs = append(contextWordIDs, wordID)
+			}
 		} else {
 			fmt.Printf("Found word %s with ID %d\n", word, wordID)
+			contextWordIDs = append(contextWordIDs, wordID)
 		}
 	}
 	return nil
